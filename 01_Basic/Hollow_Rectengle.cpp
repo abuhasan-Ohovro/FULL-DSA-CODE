801c8240b19e08758c17321e9e3c38777c56f842
abuhasan-Ohovro <abuhasanmondal24@gmail.com>
2024-04-20 23:05:48 +0600

    Basic pattern discussion in done

---
+++ 01_Basic/Hollow_Rectengle.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, cols;
+    cin >> rows >> cols;
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= cols; j++)
+        {
+            if (i == 1 || i == rows || j == 1 || j == cols)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+// Output should be like this
+/*
+7
+8
+********
+*      *
+*      *
+*      *
+*      *
+*      *
+********
+
+
+
+
+
+*/
